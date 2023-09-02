a457fe076a1788628c54a5ab0f3d6f95c2b4268e
aj-patalinghog <99686240+aj-patalinghog@users.noreply.github.com>
2023-09-02 12:51:22 -1000

    Parse args into 2D array

--- sshell.c
+++ sshell.c
@@ -18,36 +18,49 @@
 
 // #define DEBUG 1  /* In case you want debug messages */
 
-void parse_args(char *buffer, char** args, size_t args_size, size_t *nargs) {
+void parse_args(char *buffer, char *args[ARR_SIZE][ARR_SIZE], size_t args_size, size_t *nargs) {
    char *buf_args[args_size]; 
    char *wbuf = buffer;
-   buf_args[0]=buffer; 
-   args[0]=buffer;  /* First argument */
+   buf_args[0] = buffer; 
    /*
     *  The following replaces delimiting characters with '\0'. 
     *  Example:  " Aloha World\n" becomes "\0Aloha\0World\0\0"
     *  Note that the for-loop stops when it finds a '\0' or it
     *  reaches the end of the buffer.
     */   
-   for(char **cp=buf_args; (*cp=strsep(&wbuf, " \n\t")) != NULL ;){
+   for(char **cp = buf_args; (*cp = strsep(&wbuf, " \n\t")) != NULL ;){
       if((**cp != '\0') && (++cp >= &buf_args[args_size]))
          break; 
    }
 
    /* Copy 'buf_args' into 'args' */    
-   size_t j=0;
-   for(size_t i=0; buf_args[i]!=NULL; i++){ 
-      if(strlen(buf_args[i])>0)  /* Store only non-empty tokens */
-         args[j++]=buf_args[i];
+   size_t j = 0;
+   size_t k = 0;
+   for(size_t i = 0; buf_args[i] != NULL; i++){ 
+      if(strlen(buf_args[i]) > 0)  /* Store only non-empty tokens */
+         if(!strcmp(buf_args[i], "|")) {
+            args[j++][k] = NULL;
+            k = 0;
+         } else {
+            args[j][k++] = buf_args[i];
+         }
    }
 
-   *nargs=j;
-   args[j]=NULL;
+   *nargs = j;
+   args[j][k] = NULL;
+   args[j+1][0] = NULL;
+
+   for(int a = 0; a < 5; a++) {
+      for(int b = 0; b < 3; b++) {
+         printf("%-10s ", args[a][b]);
+      }
+      printf("\n");
+   }
 }
 
 int main(int argc, char *argv[], char *envp[]){
    char buffer[BUFFER_SIZE];
-   char *args[ARR_SIZE];
+   char *args[ARR_SIZE][ARR_SIZE];
 
    size_t num_args;
    pid_t pid;
@@ -57,26 +70,22 @@ int main(int argc, char *argv[], char *envp[]){
       fgets(buffer, BUFFER_SIZE, stdin); /* Read in command line */
       parse_args(buffer, args, ARR_SIZE, &num_args); 
 
-      for(int i = 0; i < num_args; i++) {
-         printf("args[%d] = %s\n", i, args[i]);
-      }
+      if(num_args == 0) continue;
+      if(!strcmp(args[0][0], "exit")) exit(0);       
 
-      if (num_args>0) {
-         if(!strcmp(args[0], "exit" )) exit(0);       
-         pid = fork();
-         if (pid){  /* Parent */
-            #ifdef DEBUG
-               printf("Waiting for child (%d)\n", pid);
-            #endif
-               pid = wait(NULL);
-            #ifdef DEBUG
-               printf("Child (%d) finished\n", pid);
-            #endif
-         } else {  /* Child executing the command */
-            if(execvp(args[0], args)) {
-               puts(strerror(errno));
-               exit(127);
-            }
+      pid = fork();
+      if(pid) {  /* Parent */
+         #ifdef DEBUG
+            printf("Waiting for child (%d)\n", pid);
+         #endif
+            pid = wait(NULL);
+         #ifdef DEBUG
+            printf("Child (%d) finished\n", pid);
+         #endif
+      } else {  /* Child executing the command */
+         if(execvp(args[0][0], args[0])) {
+            puts(strerror(errno));
+            exit(127);
          }
       }
    }    
