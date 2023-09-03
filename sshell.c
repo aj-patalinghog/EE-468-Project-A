51fc255db54084e01c4202e9715089ffe37dd4a3
aj-patalinghog <99686240+aj-patalinghog@users.noreply.github.com>
2023-09-03 11:25:36 -1000

    Formatting and comments

--- sshell.c
+++ sshell.c
@@ -16,7 +16,7 @@
 #define BUFFER_SIZE 80
 #define ARR_SIZE 80
 
-//#define DEBUG 1  /* In case you want debug messages */
+// #define DEBUG 1  // In case you want debug messages
 
 void parse_args(char *buffer, char *args[ARR_SIZE][ARR_SIZE], size_t args_size, size_t *nargs) {
    char *buf_args[args_size]; 
@@ -28,17 +28,17 @@ void parse_args(char *buffer, char *args[ARR_SIZE][ARR_SIZE], size_t args_size, 
     *  Note that the for-loop stops when it finds a '\0' or it
     *  reaches the end of the buffer.
     */   
-   for(char **cp = buf_args; (*cp = strsep(&wbuf, " \n\t")) != NULL ;){
+   for(char **cp = buf_args; (*cp = strsep(&wbuf, " \n\t")) != NULL ;) {
       if((**cp != '\0') && (++cp >= &buf_args[args_size]))
          break; 
    }
 
-   /* Copy 'buf_args' into 'args' */    
+   // Copy 'buf_args' into 2D array 'args' with each row being a separate command
    size_t j = 0;
    size_t k = 0;
-   for(size_t i = 0; buf_args[i] != NULL; i++){ 
-      if(strlen(buf_args[i]) > 0)  /* Store only non-empty tokens */
-         if(!strcmp(buf_args[i], "|")) {
+   for(size_t i = 0; buf_args[i] != NULL; i++) { 
+      if(strlen(buf_args[i]) > 0) // Store only non-empty tokens
+         if(!strcmp(buf_args[i], "|")) { // When end of command is found, go to next row in 'args'
             args[j++][k] = NULL;
             k = 0;
          } else {
@@ -46,13 +46,12 @@ void parse_args(char *buffer, char *args[ARR_SIZE][ARR_SIZE], size_t args_size, 
          }
    }
 
-   *nargs = args[0][0] == NULL ? 0 : j+1;
+   *nargs = (args[0][0] == NULL) ? 0 : j+1; // j is an index, add one to get total number
    args[j][k] = NULL;
-   args[j+1][0] = NULL;
 
    #ifdef DEBUG
    for(int a = 0; args[a][0] != NULL; a++) {
-      printf("command %d: ", a + 1);
+      printf("command %d: ", a+1);
       for(int b = 0; args[a][b] != NULL; b++) {
          printf("%s ", args[a][b]);
       }
@@ -61,47 +60,50 @@ void parse_args(char *buffer, char *args[ARR_SIZE][ARR_SIZE], size_t args_size, 
    #endif
 }
 
-int main(int argc, char *argv[], char *envp[]){
+int main(int argc, char *argv[], char *envp[]) {
    char buffer[BUFFER_SIZE];
    char *args[ARR_SIZE][ARR_SIZE];
-
    size_t num_args;
    pid_t pid;
    int fd[num_args][2];
 
-   while(1){
+   while(1) {
       printf("ee468>> "); 
-      fgets(buffer, BUFFER_SIZE, stdin); /* Read in command line */
+      fgets(buffer, BUFFER_SIZE, stdin); // Read in command line
       parse_args(buffer, args, ARR_SIZE, &num_args); 
 
       if(num_args == 0) continue;
       if(!strcmp(args[0][0], "exit")) exit(0);       
 
       for(int i = 0; i < num_args; i++) {
-         //printf("i = %d\n", i);
-         if (pipe(fd[i]) == -1) {
+         // printf("i = %d\n", i);
+         if(pipe(fd[i]) == -1) {
             perror("pipe");
             exit(1);
          }
-         pid = fork();
-         if(pid) {  /* Parent */
-            close(fd[i][1]);
+         if((pid = fork()) < 0) {
+            perror("fork");
+            exit(1);
+         } else if(pid) { // Parent
             #ifdef DEBUG
             printf("i = %d\n", i);
             printf("Waiting for child (%d)\n", pid);
             #endif
+
+            close(fd[i][1]); // Parent doesn't need write end of pipes
             pid = wait(NULL);
+
             #ifdef DEBUG
             printf("Child (%d) finished\n", pid);
             #endif
-         } else {  /* Child executing the command */
-            if(i != 0) {
-               dup2(fd[i-1][0], STDIN_FILENO);
+         } else { // Child executing the command
+            if(i != 0) { // First command doesn't need to map stdin
+               dup2(fd[i-1][0], STDIN_FILENO); // Map read end of previous command's pipe to stdin
                close(fd[i-1][0]);
             }
 
-            if(i + 1 != num_args) {
-               dup2(fd[i][1], STDOUT_FILENO);
+            if(i+1 != num_args) { // Last command prints to stdout
+               dup2(fd[i][1], STDOUT_FILENO); // Map stdout to write end of current command's pipe 
                close(fd[i][1]);
             }
 
